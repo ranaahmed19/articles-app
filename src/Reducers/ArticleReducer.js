53aa7b59783a1e27f51a6508217dbedef869e2ce
Rana Ahmed <rana.ahmed99000@gmail.com>
2020-04-14 15:27:24 +0200

    Add article reducer.

---
+++ src/Reducers/ArticleReducer.js
@@ -0,0 +1,10 @@
+import { FETCH_ARTICLE } from "../actions";
+
+export default function (state = {}, action) {
+  switch (action.type) {
+    case FETCH_ARTICLE:
+      return action.payload;
+    default:
+      return state;
+  }
+}
--- src/Reducers/CombinedReducers.js
+++ src/Reducers/CombinedReducers.js
@@ -2,11 +2,13 @@ import { combineReducers } from "redux";
 import ArticlesReducer from "./ArticlesReducer";
 import LoggedInUserReducer from "./LoggedInUserReducer";
 import UsersReducer from "./UsersReducer";
+import ArticleReducer from "./ArticleReducer";
 
 const reducers = combineReducers({
   articles: ArticlesReducer,
   loggedInUser: LoggedInUserReducer,
   users: UsersReducer,
+  article: ArticleReducer,
 });
 
 export default reducers;
