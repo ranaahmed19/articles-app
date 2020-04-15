45dd6b9d84fb1332de2ffbcf6cf7c50cd1e0c5c5
Rana Ahmed <rana.ahmed99000@gmail.com>
2020-04-15 19:15:28 +0200

    Added login button in App header.

--- src/Components/AppHeader.js
+++ src/Components/AppHeader.js
@@ -8,12 +8,15 @@ import {
 import { withRouter } from "react-router-dom";
 
 function AppHeader({ history: { push } }) {
+  const loggedinUser = localStorage.getItem(LOGGEDIN_USER_TOKEN) || "";
   const logout = () => {
     localStorage.setItem(LOGGEDIN_USER_USERNAME, "");
     localStorage.setItem(LOGGEDIN_USER_TOKEN, "");
     push(LOGIN_URL);
   };
-
+  const signin = () => {
+    push(LOGIN_URL);
+  };
   return (
     <div className="page-main">
       <Grid>
@@ -24,10 +27,17 @@ function AppHeader({ history: { push } }) {
           <Grid.Col lg="5" />
           <Grid.Col lg="2">
             <Button.List>
-              <Button onClick={logout} icon="log-out" color="primary">
-                {"  "}
-                Log out
-              </Button>
+              {loggedinUser !== "" ? (
+                <Button onClick={logout} icon="log-out" color="primary">
+                  {"  "}
+                  Log out
+                </Button>
+              ) : (
+                <Button onClick={signin} icon="log-in" color="primary">
+                  {"  "}
+                  Sign in
+                </Button>
+              )}
             </Button.List>
           </Grid.Col>
         </Grid.Row>
